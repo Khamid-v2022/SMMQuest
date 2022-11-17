56562bfbf9f3846fc8ee117b2796a64d4f944823
Khamid-v2022 <khamid.webdev@gmail.com>
2022-11-17 13:11:43 +0800

    security page bug fix

--- resources/views/content/pages/pages-account-security.blade.php
+++ resources/views/content/pages/pages-account-security.blade.php
@@ -35,7 +35,7 @@
 <div class="row">
   <div class="col-md-12">
     <ul class="nav nav-pills flex-column flex-md-row mb-3">
-      <li class="nav-item"><a class="nav-link" href="{{url('pages/account-settings-account')}}"><i class="bx bx-user me-1"></i> Account</a></li>
+      <li class="nav-item"><a class="nav-link" href="/profile"><i class="bx bx-user me-1"></i> Account</a></li>
       <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class="bx bx-lock-alt me-1"></i> Security</a></li>
     </ul>
     <!-- Change Password -->
